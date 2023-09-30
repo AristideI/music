79a9b31c0eb343be9cf5a58ca673c8e4cf03e028
AristideI <a.isingizwe@alustudent.com>
2023-09-30 15:35:20 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {}
+function App() {
+  let pro = fetch(
+    "https://musicbrainz.org/ws/2/artist?query=artist:rihanna&fmt=json"
+  );
+
+  pro.then((data) => data.json()).then((data) => console.log(data));
+  return <p>This is the best</p>;
+}
 
 export default App;
